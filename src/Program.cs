a72d45fb99567a512ecb6793a75f6f55df970202
Damir <damirzasimenko@gmail.com>
2026-06-26 17:01:37 +0300

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -4,5 +4,16 @@ class Program
     {
         // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
+        string command = Console.ReadLine();
+        var commandsList = new List<string>();
+        if (!commandsList.Contains(command))
+        {
+
+        }
+        else
+        {
+            Console.WriteLine($"{command}: command not found");
+        }
+
     }
 }
