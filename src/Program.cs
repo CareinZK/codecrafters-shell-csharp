b71c4ea7c19c42ab968eb44341f1e8eca38c2288
Damir <damirzasimenko@gmail.com>
2026-06-26 17:09:59 +0300

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -2,18 +2,20 @@ class Program
 {
     static void Main()
     {
-        // TODO: Uncomment the code below to pass the first stage
-        Console.Write("$ ");
-        string command = Console.ReadLine();
         var commandsList = new List<string>();
-        if (commandsList.Contains(command))
+        while (true)
         {
+            Console.Write("$ ");
+            string command = Console.ReadLine();
 
-        }
-        else
-        {
-            Console.WriteLine($"{command}: command not found");
-        }
+            if (commandsList.Contains(command))
+            {
 
+            }
+            else
+            {
+                Console.WriteLine($"{command}: command not found");
+            }
+        }
     }
 }
