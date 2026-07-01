5d2bf96d728521d48ba309a9bf2b70b20ccda489
Damir <damirzasimenko@gmail.com>
2026-07-02 02:53:50 +0300

    codecrafters submit [skip ci]

--- src/Autocompletion.cs
+++ src/Autocompletion.cs
@@ -66,7 +66,7 @@ public static class AutoCompletionHandler
 
         if (matches.Count != 1)
         {
-            Console.WriteLine('\u2407'); // ␇
+            Console.Write((char)7);
             return;
         }
 
